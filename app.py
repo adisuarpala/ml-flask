231c4c98f01c18ab5bcd244018cdac42a3db8faa
Adi Suarpala <adi.suarpala@gmail.com>
2020-02-07 14:10:23 +0800

    changed

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def upload():
     # process image
     img_processed = classify(img)
     # response
-    return 'Server On Air' Response (response=img_processed, status=200, mimetype="application/json") # return json string
+    return Response (response=img_processed, status=200, mimetype="application/json") # return json string
 
 
 # start server
